9db4128d4af56d4164df359e7000384a670318ce
Andrew Stonecipher <astonecipher@appliedhealth.net>
2017-01-03 15:02:20 -0600

    ngSpotify project for Angular 2.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,24 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent }  from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { HttpModule } from '@angular/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
+import { ArtistComponent } from './components/artist/artist.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, 
+            FormsModule, 
+            routing, 
+            HttpModule],
+  declarations: [AppComponent, 
+                 SearchComponent, 
+                 NavbarComponent, 
+                 AboutComponent, 
+                 ArtistComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
